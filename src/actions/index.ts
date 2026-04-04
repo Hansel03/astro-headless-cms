3e1fbf778e10f1dffaf49a9ef4b64e9c1590f13e
Hansel Suarez <hansel_suarez@bhd.com.do>
2026-04-04 16:34:16 -0700

    feat: add email subscription functionality with Resend integration

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,11 @@
+import { Resend } from "resend";
+import { FROM_EMAIL, RESEND_API_KEY, TO_EMAIL } from "astro:env/server";
+import { z } from "astro/zod";
 import { defineAction } from "astro:actions";
 import { addLike, getLikes } from "./repository";
 import type { LikesResponse } from "./model";
+
+const resend = new Resend(RESEND_API_KEY);
 
 export const server = {
   addLike: defineAction<LikesResponse>({
@@ -12,6 +17,27 @@ export const server = {
   getLikes: defineAction<LikesResponse>({
     async handler(slug) {
       return { likes: await getLikes(slug) };
+    },
+  }),
+
+  sendSuncription: defineAction({
+    accept: "form",
+    input: z.object({
+      email: z.string().email("Invalid email address"),
+    }),
+    handler: async (input) => {
+      try {
+        const { email } = input;
+        await resend.emails.send({
+          from: FROM_EMAIL,
+          to: TO_EMAIL,
+          subject: "New subscription",
+          html: `<p>New subscription from ${email}</p>`,
+        });
+        return { success: true, message: "Subscription successful" };
+      } catch (error) {
+        return { success: false, message: "Failed to send subscription email" };
+      }
     },
   }),
 };
